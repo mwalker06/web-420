c9df526b2c9c4f5989ba262606aac4029a05484d
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-05-14 13:10:47 -0500

    add assignPlayerToTeam api call to routes

--- routes/walker-team-routes.js
+++ routes/walker-team-routes.js
@@ -103,4 +103,95 @@ router.post("/teams", async (req, res) => {
     }
 });
 
+// assignPlayerToTeam
+/**
+ * @openapi
+ * /api/teams/{id}/players:
+ *   post:
+ *     tags:
+ *     - teams
+ *     description: API for adding a new player document
+ *     summary: Creates a new team document
+ *     parameters:
+ *      - name: id
+ *        in: path
+ *        description: Team ID
+ *        required: true
+ *        type: string
+ *     requestBody:
+ *       description: Player information
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *              - firstName
+ *              - lastName
+ *              - salary
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               salary:
+ *                 type: number
+ *     responses:
+ *       '200':
+ *         description: Team document
+ *      '401':
+ *         description: Invalid Team ID
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
+*/
+router.post("/teams/:id/players", async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id);
+        Team.findOne({ _id: req.params.id }, function (err, team) {
+            if (err) {
+                console.log(err);
+                res.status(501).send({
+                    message: "MongoDB Exception",
+                });
+            } else {
+                // if team is null, return 401
+                if (team === null) {
+                    console.log("Invalid team ID");
+                    res.status(401).send({
+                        message: "Invalid team ID",
+                    });
+                }else{
+                    //console.log(team);
+                    const newPlayer = {
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        salary: req.body.salary
+                    };
+                    team.players.push(newPlayer);
+                    team.save(function (err, updatedTeam) {
+                        if (err) {
+                            console.log(err);
+                            res.status(501).send({
+                                message: "MongoDB Exception",
+                            });
+                        } else {
+                            //console.log(updatedTeam);
+                            // get new player and return
+                            const player = updatedTeam.players[updatedTeam.players.length - 1];
+                            res.json(player);
+                        }
+                    });
+                }
+            }
+        });
+    } catch (err) {
+        res.status(500).send({
+            message: "Server Exception",
+        });
+    }
+});
+
+
+
 module.exports = router;
